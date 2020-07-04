866a367e5d1e5e46d05af714055ac2c28c16c82e
arielwc <arielwalkercochrane@gmail.com>
2020-07-04 18:42:22 +1200

    added ability to have random bomb allocation on board

--- minesweeper.js
+++ minesweeper.js
@@ -2,71 +2,63 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
 var board = {
-//   cells: [
-//   {
-//     row: 0,
-//     col: 0,
-//     hidden: true,
-//     isMine: false
-//   },
-//   {
-//     row: 0,
-//     col: 1,
-//     hidden: true,
-//     isMine: false
-//   },
-//   {
-//     row: 1,
-//     col: 0,
-//     hidden: true,
-//     isMine: false
-//   },
-//   {
-//     row: 1,
-//     col: 1,
-//     hidden: true,
-//     isMine: false
-//   }
-// ],
-// }
 cells: [
   {row: 0, col: 0, hidden: true, isMine: false},
-  {row: 0, col: 1, hidden: true, isMine: true},
+  {row: 0, col: 1, hidden: true, isMine: false},
   {row: 0, col: 2, hidden: true, isMine: false},
   {row: 0, col: 3, hidden: true, isMine: false},
   {row: 0, col: 4, hidden: true, isMine: false},
-  {row: 0, col: 5, hidden: true, isMine: true},
+  {row: 0, col: 5, hidden: true, isMine: false},
   {row: 1, col: 0, hidden: true, isMine: false},
   {row: 1, col: 1, hidden: true, isMine: false},
-  {row: 1, col: 2, hidden: true, isMine: true},
+  {row: 1, col: 2, hidden: true, isMine: false},
   {row: 1, col: 3, hidden: true, isMine: false},
   {row: 1, col: 4, hidden: true, isMine: false},
   {row: 1, col: 5, hidden: true, isMine: false},
   {row: 2, col: 0, hidden: true, isMine: false},
   {row: 2, col: 1, hidden: true, isMine: false},
   {row: 2, col: 2, hidden: true, isMine: false},
-  {row: 2, col: 3, hidden: true, isMine: true},
+  {row: 2, col: 3, hidden: true, isMine: false},
   {row: 2, col: 4, hidden: true, isMine: false},
   {row: 2, col: 5, hidden: true, isMine: false},
   {row: 3, col: 0, hidden: true, isMine: false},
   {row: 3, col: 1, hidden: true, isMine: false},
   {row: 3, col: 2, hidden: true, isMine: false},
-  {row: 3, col: 3, hidden: true, isMine: true},
+  {row: 3, col: 3, hidden: true, isMine: false},
   {row: 3, col: 4, hidden: true, isMine: false},
   {row: 3, col: 5, hidden: true, isMine: false},
   {row: 4, col: 0, hidden: true, isMine: false},
   {row: 4, col: 1, hidden: true, isMine: false},
-  {row: 4, col: 2, hidden: true, isMine: true},
+  {row: 4, col: 2, hidden: true, isMine: false},
   {row: 4, col: 3, hidden: true, isMine: false},
-  {row: 4, col: 4, hidden: true, isMine: true},
+  {row: 4, col: 4, hidden: true, isMine: false},
   {row: 4, col: 5, hidden: true, isMine: false},
   {row: 5, col: 0, hidden: true, isMine: false},
-  {row: 5, col: 1, hidden: true, isMine: true},
+  {row: 5, col: 1, hidden: true, isMine: false},
   {row: 5, col: 2, hidden: true, isMine: false},
   {row: 5, col: 3, hidden: true, isMine: false},
   {row: 5, col: 4, hidden: true, isMine: false},
   {row: 5, col: 5, hidden: true, isMine: false}
   ],}
+
+function getRandomNumber (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+function seedBooms () {
+  var randomIndex = getRandomNumber(0, 35)
+  var randomCell = board.cells[randomIndex]
+  randomCell.isMine = true
+}
+function doTimes(number) {
+  for (var i = 0; i < number; i++) {
+      seedBooms(i); 
+  }
+}
+
+doTimes(8);
+
+
 
 function startGame () {
   for (var i = 0; i < board.cells.length; i++){
